153ea560465836120034a278db90b8c01c12fe3e
Dashdaslaya <143828372+Dashdaslaya@users.noreply.github.com>
2023-11-17 14:55:54 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,60 @@
 // script.js
 
+let isHosting = false;
+
 function openBrowser(browser) {
-    let message;
+    let content;
 
     switch (browser) {
         case 'chrome':
-            message = "We don't have that.";
+            content = "We don't have that.";
             break;
         case 'firefox':
-            message = "Sorry, but we cannot access that.";
+            content = "Sorry, but we cannot access that.";
             break;
         case 'tor':
-            message = "Got it! Launching Tor and connecting. This may take a while...";
+            content = "Got it! Launching Tor and connecting. This may take a while...";
             // Additional code to launch Tor can be added here
             break;
         case 'hackweb':
-            message = "Launching Hack Web.";
-            // Additional code for Hack Web can be added here
+            content = `
+                <h2>Welcome to Hack Web</h2>
+                <p>Please sign in or create an account:</p>
+                <form id="hackweb-form">
+                    <label for="username">Username:</label>
+                    <input type="text" id="username" name="username" required>
+
+                    <label for="password">Password:</label>
+                    <input type="password" id="password" name="password" required>
+
+                    <button type="button" onclick="signIn()">Sign In</button>
+                    <button type="button" onclick="createAccount()">Create Account</button>
+                    
+                    <button type="button" onclick="toggleHosting()">Toggle Hosting</button>
+                    ${isHosting ? '<p>Hosting at: <strong>http://localhost:3000</strong></p>' : ''}
+                </form>
+            `;
             break;
         default:
-            message = "Invalid browser selection.";
+            content = "Invalid browser selection.";
             break;
     }
 
-    document.getElementById('browser-display').innerHTML = `<p>${message}</p>`;
+    document.getElementById('browser-display').innerHTML = content;
 }
+
+function toggleHosting() {
+    isHosting = !isHosting;
+    alert(isHosting ? "Hosting started at http://localhost:3000" : "Hosting stopped");
+}
+
+function signIn() {
+    // Add magical code for user sign-in
+    alert("Signing in! (This is where the magic happens)");
+}
+
+function createAccount() {
+    // Add magical code for account creation
+    alert("Creating account! (More magic)");
+}
+
